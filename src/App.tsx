84c86bdb2861b8cd48e42b3b241855ea1ec309c1
Duong Minh <tnmnh1105@gmail.com>
2023-06-04 16:23:53 +0700

    feat: add course detail page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 import './assets/styles/app.css'
 import './assets/styles/app.scss'
+import CourseDetail from './pages/Course/CourseDetail'
 import Home from './pages/Home/Header'
 import Login from './pages/Login/Login'
 
@@ -15,6 +16,11 @@ const App: FC = () => {
 					<Route path='/' element={<Navigate to="/login" />} />
 					<Route path="/login" Component={Login} />
 					<Route path="/home" Component={Home} />
+					<Route   path="/courses/:courseId"
+						loader={() => {
+						}}
+						action={() => {}}
+						Component={CourseDetail} />
 				</Routes>
 			</BrowserRouter>
 		</Fragment>
